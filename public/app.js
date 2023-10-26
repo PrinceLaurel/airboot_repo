08687301e609d4e19013435886fb007f06f0a7ef
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-10-26 16:04:50 +0200

    la till delete knapp, funkar

--- public/app.js
+++ public/app.js
@@ -1,4 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    const contactsTable = document.getElementById('contacts-table');
+    
+    const fetchContacts = async () => {
+        try {
+            const response = await fetch('/contacts');
+            const contacts = await response.json();
+            renderContacts(contacts);
+        } catch (error) {
+            console.error('Error fetching contacts:', error);
+        }
+    }
+
+    const renderContacts = contacts => {
+        contactsTable.innerHTML = '';  
+       
+        const thead = document.createElement('thead');
+        thead.innerHTML = `
+            <thead>
+                <tr>
+                    <th scope="col">Namn</th>
+                    <th scope="col">Epost</th>
+                    <th scope="col">Kommando</th>
+                </tr>
+             </thead>
+        `;
+
+        contactsTable.appendChild(thead);
+
+        contacts.forEach((contact, index) => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${contact.name}</td>
+                <td>${contact.email}</td>
+                <td>
+                    <button class="btn btn-primary btn-sm" onclick="editContact(${index})">Edit</button>
+                    <button class="btn btn-danger btn-sm" onclick="deleteContact('${contact.id}')">Delete</button>
+                </td>
+            `;
+            contactsTable.appendChild(row);
+        });
+    }
+
+    const deleteContact = async (id) => {
+        console.log('delete called for: ' + id);
+        try {
+            const response = await fetch(`/contacts/${id}`, {
+                method: 'DELETE'
+            });
+            await fetchContacts();  
+        } catch (error) {
+            console.error('Error deleting contact:', error);
+        }
+    }
+    
+    window.deleteContact = deleteContact;
+    fetchContacts();  
+});
+    /*
     const url = 'http://localhost:4242/contacts';
 
     fetch(url)
@@ -77,4 +135,4 @@ const addData = () => {
     populateData(contacts);
 
 };
-
+*/
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const Airtable = require('airtable');
 
 const app = express();
+//const base = new Airtable({ apiKey: 'patShrnb94DMI7BFw.5c79b99efb239663bc9d29e8075aaf9ce8483229d43ffc908ccbf65ee3f24ff0' }).base('appSO4a0KYEfoNwQT')
 const base = new Airtable({ apiKey: 'patlKBlqPGg6xh4rQ.e68f2d76aeab7d268c50cfbdfaf55085417d8be4a87c718c12cef5d9f21aad9d' }).base('apprHL0OZkQaPWmSd')
 
 app.use(bodyParser.json());
@@ -41,6 +42,39 @@ app.get('/contacts', (req, res) => {
         }
         res.json(contacts);
     });
+});
+
+app.post('/contacts', (req,res)=>{
+    base('contacts').create(req.body, function(err, record) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.json({
+            id: record.getId(),
+            fields: record.fields
+        });
+      });
+});
+
+app.delete('/contacts/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log('Asked to deleted: ' + id);
+
+    try {
+        await base('contacts').destroy(id, (err, deletedRecord) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Server Error');
+                return;
+            }
+            console.log('Airtable confirmed deletion of: ' + deletedRecord.id);
+            res.json({ deletedRecordId: deletedRecord.id });
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 });
 
 app.listen(4242, () => {
